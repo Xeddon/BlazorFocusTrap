bb5fc942fb60b3e1228d8b91c6bd3e9b4cb7d2de
Xeddon <34571845+Xeddon@users.noreply.github.com>
2021-03-14 14:23:31 +0100

    Initial

---
+++ wwwroot/scripts.js
@@ -0,0 +1,14 @@
+﻿window.blazorFocusTrap = {
+    start: (focusElementList) => {
+        if (window.blazorFocusTrap.isActive)
+            return;
+        window.blazorFocusTrap.isActive = true;
+        window.blazorFocusTrap.trap = focusTrap.createFocusTrap(focusElementList);
+        window.blazorFocusTrap.trap.activate();
+    },
+    stop: () => {
+        if (window.blazorFocusTrap.trap)
+            window.blazorFocusTrap.trap.deactivate();
+        window.blazorFocusTrap.isActive = false;
+    }
+};
